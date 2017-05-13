7697fbbc7978e69207f3dbc077b2fb6a47a1d4df
Matthew Uncapher <unky.codes@gmail.com>
2017-05-13 18:36:10 -0400

    Fixed Slider and input

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
 
+$('#size').on('change', function(){
+    $('#pen').val($('#size').val());
+});
 
+$('#pen').on('keyup', function(){
+    if($('#pen').val()>68) {
+        alert("please choose a number between 1 and 68");
+        $('#pen').val(68);
+        $('#size').val($('#pen').val())
+    }
+    else {$('#size').val($('#pen').val())
+    }
+});
 });
