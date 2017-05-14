a239f43ac3b06ab06a412b1634531be5bff7695a
Matthew Uncapher <unky.codes@gmail.com>
2017-05-14 15:07:13 -0400

    Breaking it till it works

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 $(document).ready(function(){
 
+var $size = $('#size').val();
+var wide = (500 / $size)+'px'
+var tall = (400 / $size)+'px'
+var $square = $("<div />", {
+    class: 'row',
+    css: { "width": wide, "height": tall}
+});
+var grid = function ($size) {
+    for( var i = 0; i<= 500; i+$size){
+        $square.append($square.clone());
+    }
+    //clone the temp row object with the columns to the wrapper
+    for (var i = 0; i <= 500; i+$size) {
+        $(".pad").append($square.clone());
+    }
+    }
+
 $('#size').on('change', function(){
     $('#pen').val($('#size').val());
 });
 
 $('#pen').on('keyup', function(){
-    if($('#pen').val()>68) {
-        alert("please choose a number between 1 and 68");
-        $('#pen').val(68);
+    if($('#pen').val()>64) {
+        alert("please choose a number between 1 and 64");
+        $('#pen').val(64);
         $('#size').val($('#pen').val())
     }
     else {$('#size').val($('#pen').val())
     }
 });
+
+
+
+
+
+
+
+
 });
